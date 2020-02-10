965600c57065b093f14188485c04eeb27ba8bc77
Pablo Sao <pablo_sao@outlook.com>
2020-02-10 16:43:10 -0600

    actualizando adaptación de codigo

--- knn_iris.py
+++ knn_iris.py
@@ -5,13 +5,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+
+def label_race(df):
+    
+    for i in range(len(nombres)):
+        df.loc[df['class']==nombres[i],'codigo_clase'] = codigoClass.get(nombres[i])
+
+
+# Importing the datasets
+datasets = pd.read_csv('iris_clean.csv')
+
+nombres = datasets['class'].unique().tolist()
+codigoClass = {}
+
+for i in range(len(nombres)):
+    codigoClass[nombres[i]] = i + 1
+
+
+label_race(datasets)
+
+#print(datasets.to_string())
+
+X = datasets.iloc[:, [1,4]].values
+y = datasets.iloc[:, 6].values
+
+## Agregar columna con codigo para el tipo de Iris-setosa y virginicola, versicolor
+
+
+# Convirtiendo a float los datos numericos
+datasets['sepal_length_cm'] = datasets['sepal_length_cm'].astype(float)
+datasets['sepal_width_cm'] = datasets['sepal_width_cm'].astype(float)
+datasets['petal_length_cm'] = datasets['petal_length_cm'].astype(float)
+datasets['petal_width_cm'] = datasets['petal_width_cm'].astype(float)
+datasets['codigo_clase'] = datasets['codigo_clase'].astype(int)
+
+
+
+
+
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -38,12 +72,13 @@ X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green','blue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
+    name_label = '{} - {}'.format(int(j), nombres[int(j) - 1])
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+                c = ListedColormap(('red', 'green','blue'))(i), label = name_label)
 plt.title('K-NN (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
@@ -56,12 +91,13 @@ X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+             alpha = 0.75, cmap = ListedColormap(('red', 'green','blue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
+    name_label = '{} - {}'.format(int(j), nombres[int(j) - 1])
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
+                c = ListedColormap(('red', 'green','blue'))(i), label = name_label)
 plt.title('K-NN (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
